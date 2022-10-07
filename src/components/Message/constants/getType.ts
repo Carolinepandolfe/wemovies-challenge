6f01bdcb1f7ebcfd478b13735fae03b1584b8ccf
carolpandolfe <carolinepandolfe@yahoo.com.br>
2022-10-07 19:45:21 -0300

    Create message component dynamic

---
+++ src/components/Message/constants/getType.ts
@@ -0,0 +1,13 @@
+import emptyCart from 'assets/emptyCart.png'
+import success from 'assets/success.png'
+
+export const getType = {
+	empty: {
+		message: 'Parece que não há nada por aqui :(',
+		image: emptyCart,
+	},
+	success: {
+		message: 'Compra realizada com sucesso!',
+		image: success,
+	},
+}
--- src/components/Message/index.tsx
+++ src/components/Message/index.tsx
@@ -1,18 +1,27 @@
+import { useNavigate } from 'react-router-dom'
+
 import Button from 'components/Button'
+
+import { getType } from './constants/getType'
 
 import * as Styled from './styles'
 
 type MessageProps = {
-	message: string
-	image: string
+	type: 'empty' | 'success'
 }
 
-const Message = ({ image, message }: MessageProps) => {
+const Message = ({ type }: MessageProps) => {
+	const navigate = useNavigate()
+
+	const backToHome = () => navigate('/')
+
 	return (
 		<Styled.Container>
-			<h1>{message}</h1>
-			<img src={image} alt="" />
-			<Button type="button">Voltar</Button>
+			<h1>{getType[type].message}</h1>
+			<img src={getType[type].image} alt="" />
+			<Button type="button" onClick={backToHome}>
+				Voltar
+			</Button>
 		</Styled.Container>
 	)
 }
